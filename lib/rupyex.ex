4dda823f76e72cb6d3e7d9bb7c5969142aad7760
MJ <tywf91@gmail.com>
2026-08-14 23:12:15 +0800

    init

---
+++ lib/rupyex.ex
@@ -0,0 +1,18 @@
+defmodule Rupyex do
+  @moduledoc """
+  Documentation for `Rupyex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Rupyex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/rupyex_test.exs
@@ -0,0 +1,8 @@
+defmodule RupyexTest do
+  use ExUnit.Case
+  doctest Rupyex
+
+  test "greets the world" do
+    assert Rupyex.hello() == :world
+  end
+end
